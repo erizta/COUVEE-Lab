30c3a6fb9fb58e50e84900f14d72b1646cfb9431
Erizta Alifa P <33772785+erizta@users.noreply.github.com>
2021-03-22 13:56:15 +0700

    partials

--- coffee-shop/routes/main.js
+++ coffee-shop/routes/main.js
@@ -3,7 +3,10 @@ const router = express.Router()
 
 router.get('/', (req, res, next) => {
 
-    const data = { title: "My Coffee House" }
+    const data = {
+        title: "My Coffee House",
+        description: "Welcome to our house of coffee! We hope you enjoy our broad selection of premium coffee from all over the world."
+    }
 
     res.render('home', data)
 })
