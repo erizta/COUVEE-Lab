d49588070ad9fb9786d667fbd76da8c0fd5bc14b
Erizta Alifa P <33772785+erizta@users.noreply.github.com>
2021-03-23 11:07:47 +0700

    connect db

--- coffee-shop/routes/main.js
+++ coffee-shop/routes/main.js
@@ -1,14 +1,32 @@
 const express = require('express')
 const router = express.Router()
+const controllers = require('../controllers')
 
-router.get('/', (req, res, next) => {
+router.get('/', async (req, res, next) => {
+    const data = req.context
+    const itemCtr = controllers.item.instance()
+    data.coffee = await itemCtr.get({ category: 'coffee' })
+    data.tea = await itemCtr.get({ category: 'tea' })
+    data.pastries = await itemCtr.get({ category: 'pastries' })
 
-    res.render('home', req.context)
+    res.render('home', data)
 })
 
 router.get('/blog', (req, res, next) => {
 
     res.render('blog', req.context)
+})
+
+router.get('/items', async (req, res, next) => {
+
+    const filters = req.query
+    const itemCtr = controllers.item.instance()
+    const items = await itemCtr.get(filters)
+
+    res.json({
+        items
+    })
+
 })
 
 module.exports = router
